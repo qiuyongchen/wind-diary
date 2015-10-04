3b93e20598c0dd5595652409c975f2c19a460a08
qiuyongchen <qiuych3@mail2.sysu.edu.cn>
2015-10-04 17:39:52 +0800

    change the color of status bar(for kitkat)

--- app/src/main/java/com/qiuyongchen/diary/MainActivity.java
+++ app/src/main/java/com/qiuyongchen/diary/MainActivity.java
@@ -1,15 +1,27 @@
 package com.qiuyongchen.diary;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.qiuyongchen.diary.widget.SystemBarTintManager;
 import com.umeng.analytics.MobclickAgent;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private View mDecor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setStatusStyle();
+
         setContentView(R.layout.activity_main);
     }
 
@@ -23,5 +35,28 @@ public class MainActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
         MobclickAgent.onPause(this);
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    private void setStatusStyle() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.colorPrimaryDark);
     }
 }
