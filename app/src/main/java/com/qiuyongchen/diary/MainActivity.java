a7ce7e448783f55329cdebfd79dc685ff2f1fac3
qiuyongchen <qiuych3@mail2.sysu.edu.cn>
2015-10-04 16:43:51 +0800

    add umeng ananlytics, and it works correctly.

--- app/src/main/java/com/qiuyongchen/diary/MainActivity.java
+++ app/src/main/java/com/qiuyongchen/diary/MainActivity.java
@@ -3,11 +3,25 @@ package com.qiuyongchen.diary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.umeng.analytics.MobclickAgent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 }
